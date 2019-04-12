47641f7e4a8766e5716ad07be4c9918cd8a05b0e
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-04-11 20:23:01 -0700

    Create Cacti_install.sh

---
+++ Cacti_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Installes a number of packages, including mariadb, httpd, php and so on
+
+yum -y install cacti  
+
+# The mysql/mariadb client installs with the cacti stack but not the server
+# If you want to have multiple cacti nodes, considder using the client and connecting
+# to another server
+
+yum -y install mariadb-server   
+comment out  line 32 ----> rpm -ql cacti|grep cacti.sql 
+comment line 38 --->
