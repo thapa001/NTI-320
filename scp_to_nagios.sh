d64f840ea24d3bab7ca0205ad157cf212f2c8642
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-05-03 14:27:41 -0700

    Update scp_to_nagios.sh

--- scp_to_nagios.sh
+++ scp_to_nagios.sh
@@ -2,6 +2,6 @@
 bash generate_config.sh $1 $2
 gcloud compute scp --zone us-east4-c $1.cfg mailatpradip8@nagios-a:/etc/nagios/servers
 usermod -a -G nagios mailatpradip8
-chmod 775 /etc/nagios/servers
+chmod 777 /etc/nagios/servers
 usermod -a -G nagios mailatpradip
 gcloud compute ssh --zone us-east4-c mailatpradip8@nagios-a --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
