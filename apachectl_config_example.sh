7534120727e6143ae52852b93c338126375248a9
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-05-30 20:22:36 -0700

    Create apachectl_config_example.sh

---
+++ apachectl_config_example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+status=$(apachectl configtest 2>&1)
+status2=$(apachectl)
+
+if [ "$status" == "Syntax OK"   ]; then
+    echo "STATUS:OK"
+    exit 0;
+
+  elif [ "status" != "Syntax OK"  ]; then
+    echo "STATUS:Critical"
+    exit 2;
+
+  elif [ "status2" != "Syntax Ok"  ] ; then
+    echo "STATUS:Warning"
+    exit 1;
+
+else
+   echo "STATUS:graceful-stop"
+   exit 3;
+fi
