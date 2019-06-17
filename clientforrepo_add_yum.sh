d33a302f5f0b381c5c63ba43b20cbf8576d12c35
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-06-16 22:02:03 -0700

    Update clientforrepo_add_yum.sh

--- clientforrepo_add_yum.sh
+++ clientforrepo_add_yum.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# this script add a yum repo on a given server/client.
-#add to existing repos (ls /etc/yum.repos.d)
-##repo server ip and we need to add our own ip address of server.
+# this is a script to add a yum repo on a given server.
 
-ip="10.150.0.15"                                                   
+ip="internal ip"                                                    # Please replace with your own IP
 
-#vim /etc/yum.repos.d/epel.repo
 echo "[nti-320]
 name=Extra Packages for Centos from NTI-320 7 - $basearch
 #baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
@@ -14,17 +11,14 @@ baseurl=http://$ip/centos/7/extras/x86_64/Packages/
 enabled=1
 gpgcheck=0
 " >> /etc/yum.repos.d/NTI-320.repo                                  
-
-# Now that the repo is added, list all repos and make sure
-# it shows up without error
+                                                                    # Now that the repo is added, list all repos and make sure
+                                                                    # it shows up without error
 yum repolist
+
 isokay=`yum search "hello world" | grep "Matched: hello world"`
+
 if [ -z "$isokay" ]; then
    echo "There's somthing wrong with your repo... check yum repolist to see if it shows up then try installing a package"
 else
    echo "All is well and we found your test package: $isokay"   
 fi
-#After scripting vim file we can run these things to check whether it is working or not
-#bash add_yum_repo.sh
-#yum search "helloworld"
-#yum -y install helloworld
